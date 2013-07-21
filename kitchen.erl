6389aaac8a81b37786864d241b5107a49395e3c7
Rustem Kamun <r.kamun@gmail.com>
2013-07-21 13:41:02 +0600

    reminder app

---
+++ kitchen.erl
@@ -0,0 +1,68 @@
+-module(kitchen).
+-compile(export_all).
+
+start(FoodList) ->
+    spawn(?MODULE, fridge2, [FoodList]).
+
+% protocol
+store(Pid, Food) ->
+    Pid ! {self(), {store, Food}},
+    receive
+        {Pid, Msg} -> Msg
+    end.
+
+take(Pid, Food) ->
+    Pid ! {self(), {take, Food}},
+    receive
+        {Pid, Msg} -> Msg
+    end.
+
+% protocol with hanging timeout (prevents deadlock occurence)
+store2(Pid, Food) ->
+    Pid ! {self(), {store, Food}},
+    receive
+        {Pid, Msg} -> Msg
+    after 3000 ->
+        timeout
+    end.
+
+take2(Pid, Food) ->
+    Pid ! {self(), {take, Food}},
+    receive
+        {Pid, Msg} -> Msg
+    after 3000 ->
+        timeout
+    end.
+
+%without state: naive
+fridge() ->
+    receive
+        {From, {store, _Food}} ->
+            From ! {self(), ok},
+            fridge();
+        {From, {take, _Food}} ->
+            From ! {self(), not_found},
+            fridge();
+        terminate ->
+            ok
+    end.
+
+% with state
+fridge2(FoodList) ->
+    receive
+        {From, {store, Food}} ->
+            From ! {self(), ok},
+            fridge2([Food|FoodList]);
+        {From, {take, Food}} ->
+            case lists:member(Food, FoodList) of
+                true ->
+                    From ! {self(), {ok, Food}},
+                    fridge2(lists:delete(Food, FoodList));
+                false ->
+                    From ! {self(), not_found},
+                    fridge2(FoodList)
+            end;
+        terminate ->
+            ok
+    end.
+
